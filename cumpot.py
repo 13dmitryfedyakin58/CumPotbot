5b72e90a59bf5e6c5a37a1b4adfbdcb815321b35
Jacque Fresco <74616517+13dmitryfedyakin58@users.noreply.github.com>
2020-11-18 18:20:37 +0300

    Update cumpot.py

--- cumpot.py
+++ cumpot.py
@@ -2,7 +2,9 @@ import telebot
 from pornhub_api import PornhubApi
 import random
 from telebot import types
-bot = telebot.TeleBot('1360141455:AAEz5hbGuCg4Mv5Aqa4bHRT1kcvQOVHIRSI');
+bot = telebot.TeleBot('1360141455:AAEz5hbGuCg4Mv5Aqa4bHRT1kcvQOVHIRSI')
+api = PornhubApi()
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/help":
@@ -11,7 +13,6 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Напиши категорию")
         bot.register_next_step_handler(message, get_tag_messages)
     elif message.text == "рандом":
-        api = PornhubApi()
         count = 0
         tags = random.sample(api.video.tags("f").tags, 5)
         category = random.choice(api.video.categories().categories)
@@ -25,17 +26,30 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'more':
+        bot.send_message(call.message.chat.id, "Напиши категорию")
+        bot.register_next_step_handler(call.message, get_tag_messages)
+    #    bot.register_next_step_handler(call.message, get_tag_messages)
+
 def get_tag_messages(message):
     count = 0
-    api = PornhubApi()
     data = api.search.search(
         ordering="mostviewed",
         period="weekly",
         tags=[message.text],
     )
-    for vid in data.videos:
-        if count == 3:
-            break
+    for i in range(2):
+        vid = random.choice(data.videos)
         bot.send_message(message.from_user.id, vid.url)
-        count += 1
+    #Добавление кнопки "ещё"
+    markup = types.InlineKeyboardMarkup()
+    btn_my_site= types.InlineKeyboardButton(text='Ещё', callback_data='more')
+    markup.add(btn_my_site)
+
+    vid = random.choice(data.videos)
+    bot.send_message(message.from_user.id, vid.url, reply_markup = markup)
+
+
 bot.polling(none_stop=True, interval=0)
